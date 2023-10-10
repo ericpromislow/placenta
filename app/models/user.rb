ff98ce2952fb4f2b53f3abc00e56e50df992f81d
Eric Promislow <eric.promislow@suse.com>
2023-10-09 22:15:50 -0700

    testing the user model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   # For temporary users, assign email of +++TEMP+++<invitee email>, username=ADJ-NOUN, password=random-16-bytes
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
-  validates :email, presence: true, length: { maximum: 150 }, format: { with: /.+@.+\.\w/, uniqueness: true }
+  validates :email, presence: true, length: { maximum: 150 },
+    format: { with: /.+@.+\.\w/ }, uniqueness: true
+
   has_secure_password
   before_save :check_temporaries
   def check_temporaries
     if self.is_temporary.nil?
       self.is_temporary = false
     end
+    self.email = email.downcase
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'faker'
 
 class UserTest < ActiveSupport::TestCase
   def setup
@@ -8,6 +9,12 @@ class UserTest < ActiveSupport::TestCase
 
   test "basic is valid" do
     assert @user.valid?
+  end
+
+  test "space not good" do
+    @user.username = '     ';
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "Username can't be blank"
   end
 
   test "needs password" do
@@ -26,4 +33,29 @@ class UserTest < ActiveSupport::TestCase
       password: "foobar", password_confirmation: "foobar")
     assert_not u.valid?
   end
+
+  test "email too long" do
+    @user.email = 'x' * 150 + '@cheeps.com'
+    assert_not  @user.valid?
+    assert_includes @user.errors.full_messages, "Email is too long (maximum is 150 characters)"
+  end
+
+  test "email in use" do
+    user1 = User.all[0]
+    username = Faker::Name.name
+    password = Faker::Internet.password
+    user = User.new(username: username, email: user1.email, password: password, password_confirmation: password)
+    user.save
+    assert_not  user.valid?
+    assert_includes user.errors.full_messages, "Email has already been taken"
+  end
+
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
