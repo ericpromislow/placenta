cf2605e5b5448152202de4152587076533fb88f0
Eric Promislow <eric.promislow@suse.com>
2023-10-09 21:00:02 -0700

    tweak the user model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 150 }, format: { with: /.+@.+\.\w/, uniqueness: true }
   has_secure_password
+  before_save :check_temporaries
+  def check_temporaries
+    if self.is_temporary.nil?
+      self.is_temporary = false
+    end
+  end
 end
