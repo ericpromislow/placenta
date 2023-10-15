2ced9ef67346bec88bb208825b45fae5ca86bb14
Eric Promislow <eric.promislow@suse.com>
2023-10-14 19:03:19 -0700

    Add some more user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   # For temporary users, assign email of +++TEMP+++<invitee email>, username=ADJ-NOUN, password=random-16-bytes
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 150 },
-    format: { with: /.+@.+\.\w/ }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 4 }
+    format: { with: /.+@.+\.\w/ }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 4 }
 
   has_secure_password
+
   before_save :check_temporaries
+
   def check_temporaries
     if self.is_temporary.nil?
       self.is_temporary = false
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,13 +11,19 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "longer password is valid" do
+    # Don't know where 72 comes from (yet)
+    @user.password_confirmation = @user.password = 'x' * 72
+    assert @user.valid?
+  end
+
   test "space not good" do
     @user.username = '     ';
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "Username can't be blank"
   end
 
-  test "needs password" do
+  test "needs password matching" do
     @user.password_confirmation += ' '
     assert_not @user.valid?
   end
@@ -26,6 +32,19 @@ class UserTest < ActiveSupport::TestCase
     @user.password_confirmation = @user.password = '123'
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "Password is too short (minimum is 4 characters)"
+  end
+
+  test "password too long" do
+    @user.password_confirmation = @user.password = 'x' * 73
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "Password is too long (maximum is 72 characters)"
+  end
+
+  test "password can be all-white-space" do
+    @user.password = @user.password_confirmation = " " * 12
+    assert @user.valid?
+    @user.password = @user.password_confirmation = " \t\r\n" * 12
+    assert @user.valid?
   end
 
   test "missing name" do
@@ -38,7 +57,17 @@ class UserTest < ActiveSupport::TestCase
     u = User.new(username: "fsda", email: "horkye",
       password: "foobar", password_confirmation: "foobar")
     assert_not u.valid?
+    end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@.com @bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
+
 
   test "email too long" do
     @user.email = 'x' * 150 + '@cheeps.com'
@@ -56,12 +85,30 @@ class UserTest < ActiveSupport::TestCase
     assert_includes user.errors.full_messages, "Email has already been taken"
   end
 
+  test "username in use" do
+    user1 = User.all[0]
+    email = Faker::Internet.email
+    password = Faker::Internet.password
+    user2 = User.new(username: user1.username, email: email, password: password, password_confirmation: password)
+    assert_not user2.valid?
+    assert_includes user2.errors.full_messages, "Username has already been taken"
+  end
 
   test "email addresses should be saved as lowercase" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "upper-case email in use" do
+    user1 = User.all[0]
+    username = Faker::Name.name
+    password = Faker::Internet.password
+    user = User.new(username: username, email: user1.email.upcase, password: password, password_confirmation: password)
+    user.save
+    assert_not  user.valid?
+    assert_includes user.errors.full_messages, "Email has already been taken"
   end
 
 end
