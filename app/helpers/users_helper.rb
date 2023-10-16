602196743ed0bd1e541c3b8f8ce222f679c71c34
Eric Promislow <eric.promislow@suse.com>
2023-10-15 20:28:38 -0700

    more user signup stuff

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # Not actually implementing this (it's 2023 - I know gravatars still work,
+  # but it seems so unlikely that they still do)
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    size         = options[:size]
+    gravatar_id  = Digest::MD5::hexdigest('eric.promislow@gmail.com')
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid info doesn't create a user" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { username: '',
+          email: 'u@i',
+          password: 'abc',
+          password_confirmation: 'defghi'
+        }
+      }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation .alert', 'The form contains 4 errors.'
+    assert_select 'div#error_explanation ul li[1]', "Username can't be blank"
+    assert_select 'div#error_explanation ul li[2]', "Email is invalid"
+    assert_select 'div#error_explanation ul li[3]', "Password is too short (minimum is 4 characters)"
+    assert_select 'div#error_explanation ul li[4]', "Password confirmation doesn't match Password"
+  end
+
+  test "valid user info adds the user" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        username: 'charlie1',
+        email: 'charlie1u@i.com',
+        password: 'charlie1',
+        password_confirmation: 'charlie1'
+      }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select '.alert-success', 'Welcome, charlie1'
+  end
+
+end
